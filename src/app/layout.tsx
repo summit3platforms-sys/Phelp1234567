2abf2a8a85d171f03fd4972fc6d12aa034f3e32d
Kuldeep Singh <agentkuldeep@Kuldeeps-MacBook-Pro-2.local>
2026-07-20 19:05:27 +0530

    chore: remove Tawk.to live chat widget

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,20 +36,7 @@ export default function RootLayout({
             gtag('config', 'G-HW217Z2WG0');
           `}
         </Script>
-        {/* Tawk.to Script */}
-        <Script id="tawk-to" strategy="lazyOnload">
-          {`
-            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-            (function(){
-            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-            s1.async=true;
-            s1.src='https://embed.tawk.to/6a54f67265dcf91d49126d8c/1jtdu5d2m';
-            s1.charset='UTF-8';
-            s1.setAttribute('crossorigin','*');
-            s0.parentNode.insertBefore(s1,s0);
-            })();
-          `}
-        </Script>
+
       </head>
       <body>
         <header className="header">
